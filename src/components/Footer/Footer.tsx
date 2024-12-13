effe5f42998555ba515a765a44dccf974fd100b6
Dahyeon Jin <wlsekgus12@gmail.com>
2024-12-14 04:03:16 +0900

    style: Footer 반응형 적용

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,14 +9,15 @@ const Footer = () => {
       <Link href="/">
         <Image src={logo} alt="모멘티아 로고" width={45} priority />
       </Link>
-      <div className="flex flex-wrap wrap-reverse items-center mt-12">
+
+      <div className="flex flex-wrap wrap-reverse items-center mt-10">
         <div className="w-full">
-          <p className="mb-3">Exhibitions, for Artists' Moments</p>
-          <div className="flex items-center justify-between">
+          <p className="mb-2">Exhibitions, for Artists' Moments</p>
+          <div className="flex flex-col justify-between md:flex-row">
             <p>예술가들을 위한 순간, 모멘티아</p>
-            <div className="flex gap-16">
-              <span>이용약관</span>
-              <span>개인정보처리방침</span>
+            <div className="flex md:gap-16 gap-10 md:mt-0 mt-8">
+              <button>이용약관</button>
+              <button>개인정보처리방침</button>
             </div>
           </div>
         </div>
