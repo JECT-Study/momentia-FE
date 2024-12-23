a7cc4874e8e546b20f89eafd033fb2f3292cd8de
Dahyeon Jin <wlsekgus12@gmail.com>
2024-12-24 04:13:41 +0900

    style: 아이콘 추가 (화살표, 잠금)

---
+++ src/components/Icon/icons/DropUp.tsx
@@ -0,0 +1,17 @@
+const DropUp = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={className}
+      viewBox='0 0 18 18'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M8.20759 5.02937C8.6079 4.50936 9.3921 4.50936 9.79241 5.02937L14.6888 11.39C15.195 12.0476 14.7263 13 13.8964 13H4.10358C3.27374 13 2.80497 12.0476 3.31117 11.39L8.20759 5.02937Z'
+        fill='currentColor'
+      />
+    </svg>
+  );
+};
+
+export default DropUp;
